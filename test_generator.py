d499b29cf97ade6e1c7b0f2cc2ef2d7b1043256b
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-01-26 13:33:06 +0000

    26/01/21
    circular, vaucouleurs profile

--- test_generator.py
+++ test_generator.py
@@ -13,6 +13,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 popt = np.loadtxt('image_parameters.txt')
 
+maxAmp = 4500# np.random.randint(4500,4600)
+b = 0.1
+radius1 = (-1*(1/b)*np.log((popt[1]+10*popt[2])/maxAmp))**4
+	
 basearray = np.zeros((image_size,image_size*2)) #test image, asymmetrical to check indices
 #make background with gaussian distribution same as real image
 for i in range(image_size):
@@ -21,30 +25,36 @@ for i in range(image_size):
 		
 #assign location to galaxies, but ensuring they're not too close to edges
 for i in range(no_objects):
-	object_loc.append(((np.random.randint(15,image_size-15)),(np.random.randint(15,image_size*2-15))))
+	object_loc.append(((np.random.randint(radius1,(image_size)-radius1)),(np.random.randint(radius1,(image_size*2)-radius1))))
 	i += 1
 	
-def gaussian2d(a, mux, muy, sigma,x, y):
-	gauss = a*np.exp(-((x-mux)**2/(2*sigma**2))-((y-muy)**2/(2*sigma**2)))
-	return gauss
+#def gaussian2d(a, mux, muy, sigma,x, y):
+#	gauss = a*np.exp(-((x-mux)**2/(2*sigma**2))-((y-muy)**2/(2*sigma**2)))
+#	return gauss
 
 def vaucouleurIntensity(amplitude, radius, b):
 	intensity = amplitude*np.exp(-b*(radius**0.25))
 	return intensity
 
 for i in range(no_objects):
-	loctemp=object_loc[i]
+	loctemp=object_loc[i] 
 	x_centre=loctemp[0]
 	y_centre=loctemp[1]
-	maxAmp = np.random.randint(5000,6000)
-	b = 0.1
-	radius1 = (-1*(1/b)*np.log10((popt[1]+4*popt[2])/maxAmp))**4
+#	maxAmp = 5000#np.random.randint(5000,6000)
+#	b = 0.1
+#	radius1 = (-1*(1/b)*np.log((popt[1]+4*popt[2])/maxAmp))**4
+	print(radius1)
 	radius1 = int(radius1)
 	for j in range(x_centre-radius1,x_centre+radius1): #6 is the radius, arbitrary can be changed
 		for k in range(y_centre-radius1,y_centre+radius1):
+#			print(j)
+#			print(k)
 			radius2 = np.sqrt((j-x_centre)**2+(k-y_centre)**2)
-			basearray[j,k] = vaucouleurIntensity(maxAmp, radius2, b) #makes the objects gaussian blobs, arbitrary amplitude and sigma, can be changed
+			if radius2<=radius1:
+				basearray[j,k] = vaucouleurIntensity(maxAmp, radius2, b)
 
+			  #makes the objects gaussian blobs, arbitrary amplitude and sigma, can be changed
+ 
 plt.figure()
 plt.imshow(basearray)
 
