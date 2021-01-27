5cc5090d601e29009fd957005ed35294cd5dde2f
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-01-27 15:27:21 +0000

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -4,25 +4,17 @@ Created on Sun Jan 24 15:58:32 2021
 
 @author: maria
 """
-from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
-from scipy.optimize import curve_fit
 galaxyCounts=np.loadtxt('galaxycounts.txt') #some of these are negative rip
 galaxyLocation=np.loadtxt('galaxylocations.txt')
 
 ZP=2.530E+01 #taken from header
-
+ZP_err = 2.0E-2 #taken from header of FITS file
 magnitudes = []
 for i in galaxyCounts:
 	tempMag = ZP-(2.5*np.log10(i))
 	magnitudes.append(tempMag)
-	
-yerr = []
-for i in range(len(galaxyCounts)):
-	yerr.append(np.sqrt(galaxyCounts[i]))
-#need some error propagation
 	
 numberSmallerMag = []
 for i in range(len(magnitudes)):
