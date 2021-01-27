11b735561ed3d6847df5b8247b6bf09ffd95c7f8
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-01-27 19:06:49 +0000

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -6,10 +6,12 @@ Created on Sun Jan 24 15:58:32 2021
 """
 import numpy as np
 import matplotlib.pyplot as plt
-galaxyCounts=np.loadtxt('fixed_r galaxy counts.txt')
+#galaxyCounts=np.loadtxt('fixed_r galaxy counts.txt')
 #galaxyCounts=np.loadtxt('variable_r galaxy counts.txt')
 #galaxyCounts=np.loadtxt('galaxy_counts_plmv270121.txt')
-galaxyLocation=np.loadtxt('galaxy_locations_plmv270121.txt')
+galaxyCounts=np.loadtxt('variable_r galaxy counts - q = 2,5.txt')
+
+#galaxyLocation=np.loadtxt('galaxy_locations_plmv270121.txt')
 
 ZP=2.530E+01 #taken from header
 ZP_err = 2.0E-2 #taken from header of FITS file
@@ -25,15 +27,22 @@ for i in range(len(magnitudes)):
 	#print(b) #a bunch of these are zero oops
 	numberSmallerMag.append(b)
 	
-#plt.errorbar(magnitudes, np.log10(numberSmallerMag), yerr, 'x')
 plt.plot(magnitudes, np.log10(numberSmallerMag),'x')
+plt.grid()
+plt.xlabel("Magnitude", fontsize=15)
+plt.ylabel("log(N)", fontsize=15)
+plt.tick_params(labelsize=12)
 
 #choose indices of linear region manually by examining plot
-p = np.polyfit(magnitudes[12:1005], np.log10(numberSmallerMag)[12:1005],1)
+linearStartIndex = 8
+linearEndIndex = 800
+
+p = np.polyfit(magnitudes[linearStartIndex:linearEndIndex], np.log10(numberSmallerMag)[linearStartIndex:linearEndIndex],1)
 fitPoints = []
-for i in range(len(magnitudes)):
+for i in range(linearStartIndex, linearEndIndex):
     fitPoints.append(p[0]*magnitudes[i]+p[1])
 	
-plt.plot(magnitudes,fitPoints)
+plt.plot(magnitudes[linearStartIndex:linearEndIndex],fitPoints, label="variable r, q=2.5 , gradient = "+str(p[0]))
+plt.legend()
 print("the gradient of the linear part is ",p[0])
 
