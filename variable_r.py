e744dca0cce962515acbe1cba844d9128235e21b
FZK98 <77396929+FZK98@users.noreply.github.com>
2021-01-28 11:30:49 +0000

    Update variable_r.py

--- variable_r.py
+++ variable_r.py
@@ -33,6 +33,7 @@ popt = np.loadtxt('image_parameters.txt')
 mask = np.loadtxt('mask.txt')
 #this is the sample of the mask that corresponds to the image
 masktest =  mask[1500:1650,1900:2000] #[y,x]
+objectmask = np.ones((testimagey, testimagex))
 #displays the mask sample to show is corresponds to the image
 plt.figure()
 plt.imshow(masktest)
@@ -46,6 +47,7 @@ plt.show()
 datatest1d=datatest.ravel()
 #sort the data into ascending order
 datatest1d_sorted = sorted(datatest1d)
+datatest1d_sorted = list(filter(lambda a: a!=0, datatest1d_sorted ))
 
 #sums values of pixels inside 1st aperture, pixels of galaxy
 def galaxyPhotons(x_centre,y_centre, radius):
@@ -54,7 +56,7 @@ def galaxyPhotons(x_centre,y_centre, radius):
     for i in range(x_centre-radius,x_centre+radius):
         for j in range(y_centre-radius,y_centre+radius):
             #if the index is still inside our sample image and the mask is 1 (i.e. readable) 
-            if i<testimagex and i>=0 and j<testimagey and j>=0 and masktest[j,i]: #only consider pixels within the image!
+            if i<testimagex and i>=0 and j<testimagey and j>=0 and objectmask[j,i] == 1: #only consider pixels within the image!
                 #d is the distance fromthe image 
                 d=np.sqrt((i-x_centre)**2+(j-y_centre)**2)
                 #if d is smaller than the radius
@@ -63,6 +65,7 @@ def galaxyPhotons(x_centre,y_centre, radius):
                     photon_vals.append(data[j,i])
                     #scanned pixels get marked off so they aren't rescanned
                     masktest[j,i] = 0 
+                    objectmask[j,i] = 0
     #return a sum of all the photon values and the number of pixels scanned
     return(np.sum(photon_vals), len(photon_vals))
 
@@ -75,17 +78,17 @@ def localBackground(x_centre,y_centre, initialRadius, secondaryRadius):
                 #diameter of the secondary apeture
                 d2=np.sqrt((i-x_centre)**2+(j-y_centre)**2)
                 #take only points between the two rings 
-                if initialRadius <= d2 and d2<=secondaryRadius and masktest[j,i] == 1:
+                if initialRadius <= d2 and d2<=secondaryRadius and objectmask[j,i] == 1:
                     #the pixel is added to the list 
                     bg_photon_vals.append(data[j,i])
                     masktest[j,i] = 0 
     #return the average pixel value from the outer apeture
-    return(np.sum(bg_photon_vals)/len(bg_photon_vals))
+    return(np.mean(bg_photon_vals), np.std(bg_photon_vals))
 
 def pixelPos(data, value):
     indices = np.where(datatest==value) #gives tuples of coordinates
     listIndices= list(zip(indices[0], indices[1])) # gives pairs of coordinates in form (x,y)
-    print(listIndices)
+    #print(listIndices)
     return listIndices
 
 def initialRad(counts):
@@ -96,26 +99,46 @@ def initialRad(counts):
 # initialRadius = 6
 # secondaryRadius = initialRadius*1.5
 galaxyCounts = []
+galaxyCountsErr = []
 galaxyLocation = []
 initial_rad_list = []
-q = 2
+#ration between r1 and r2 
+q = 2.5
 for i in range(len(datatest1d_sorted)): 
     tempPixVal = datatest1d_sorted[-(1+i)] #find highest pixel value
     if tempPixVal > popt[1]+4*popt[2]:
         tempPixPos = pixelPos(datatest, tempPixVal) #find position of highest pixel value
         for j in range(len(tempPixPos)): #potentially multiple locations with same pixel value
             loc = tempPixPos[j]
-            if masktest[loc[0],loc[1]] == 1: #if pixel is available to use 
-                initialRadius = int(initialRad(tempPixVal))
-                if initialRadius >= 3:
+            if masktest[loc[0],loc[1]] == 1 and objectmask[loc[0],loc[1]] == 1: #if pixel is available to use 
+                initialRadius = int(initialRad(tempPixVal)) #calculate the radius based on the pixel val
+                if initialRadius >= 3: #exclude radii smaller than 3
                     initial_rad_list.append(initialRadius)
+                    #reads the flux sum and number of pixels in the galaxy
                     galaxyBrightness=galaxyPhotons(loc[1],loc[0],initialRadius)
-                    secondaryRadius = int(initialRadius*q)
+                    secondaryRadius = int(initialRadius*q) #calculates the outer radius
+                    #reads the avg background pixel val and the std dev of the background
                     galaxyBackground = localBackground(loc[1],loc[0],initialRadius, secondaryRadius)
-                    galaxyCounts.append(galaxyBrightness[0]-(popt[1]*galaxyBrightness[1]))
-                    #galaxyCounts.append(galaxyBrightness[0]-(galaxyBackground*galaxyBrightness[1]))
-                    galaxyLocation.append(loc)
-                    print(len(datatest1d_sorted) -i)
+                    #adds the corrected flux value of the galaxy to a list 
+                    galaxyCounts.append(galaxyBrightness[0]-(galaxyBackground[0]*galaxyBrightness[1]))
+                    #adds the error of the galaxy flux to a list
+                    galaxyCountsErr.append(galaxyBackground[1]*galaxyBrightness[1])
+                    galaxyLocation.append(loc) #stores the position of the galaxy
+                    print(len(datatest1d_sorted) -i) #counter for n. iterations remaining
+
+def Magnitude(galaxyflux, galaxyflux_err):
+    m = []
+    m_err = []
+    ZPinst = 25.3
+    ZPinst_err = 2e-2
+    for i in range(len(galaxyflux)):
+        m.append(ZPinst - 2.5*np.log10(galaxyflux[i]))
+        merr = np.sqrt(ZPinst_err**2 + (2.5**2)*(0.434**2)*((galaxyflux_err[i]/galaxyflux[i])**2))
+        m_err.append(merr)
+    return(m, m_err)
+    
+galaxyMagnitude = Magnitude(galaxyCounts, galaxyCountsErr)
+
 
 
 # plt.figure()
@@ -137,8 +160,11 @@ plt.imshow(masktest)
 plt.show()
 
 np.savetxt("variable_r galaxy counts.txt", galaxyCounts)
+np.savetxt("variable_r galaxy count err.txt", galaxyCountsErr)
 np.savetxt("variable_r galaxy locations.txt", galaxyLocation)
 np.savetxt("variable_r radius lengths.txt", initial_rad_list)
+np.savetxt("variable_r galaxy magnitudes", galaxyMagnitude[0])
+np.savetxt("Variable_r galaxy magnitude errors", galaxyMagnitude[1])
 
 #ZP=2.530E+01
 #def calculateMagnitude(data_points, ZP):
