ed054a94dd9674a03d194382417aa720b56d7a6c
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-01-28 15:02:26 +0000

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -6,6 +6,8 @@ Created on Sun Jan 24 15:58:32 2021
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
+from scipy.optimize import curve_fit
 #galaxyCounts=np.loadtxt('fixed_r galaxy counts.txt')
 #galaxyCounts=np.loadtxt('variable_r galaxy counts.txt')
 #galaxyCounts=np.loadtxt('galaxy_counts_plmv270121.txt')
@@ -23,12 +25,12 @@ magnitudes=np.sort(magnitudes)
 
 bins1=np.arange(np.min(magnitudes), np.max(magnitudes), 0.2)
 
-numberSmallerMag = []
-for i in range(len(magnitudes)):
-	a=np.where(magnitudes<=magnitudes[i])
-	b=len(a[0])
-	#print(b) #a bunch of these are zero oops
-	numberSmallerMag.append(b)
+#numberSmallerMag = []
+#for i in range(len(magnitudes)):
+#	a=np.where(magnitudes<=magnitudes[i])
+#	b=len(a[0])
+#	#print(b) #a bunch of these are zero oops
+#	numberSmallerMag.append(b)
 	
 numberSmallerMag2 = []
 for i in range(len(bins1)):
@@ -45,16 +47,20 @@ plt.xlabel("Magnitude", fontsize=15)
 plt.ylabel("log(N)", fontsize=15)
 plt.tick_params(labelsize=12)
 
-#choose indices of linear region manually by examining plot
-linearStartIndex = 8
-linearEndIndex = 800
+def linear(x, m , c):
+	return((m*x)+c)
 
-p = np.polyfit(magnitudes[linearStartIndex:linearEndIndex], np.log10(numberSmallerMag)[linearStartIndex:linearEndIndex],1)
+#choose indices of linear region manually by examining plot
+linearStartIndex = 6
+linearEndIndex = 34
+
+#p = np.polyfit(magnitudes[linearStartIndex:linearEndIndex], np.log10(numberSmallerMag)[linearStartIndex:linearEndIndex],1)
 fitPoints = []
+p,q=sp.optimize.curve_fit(linear, bins1[linearStartIndex:linearEndIndex], np.log10(numberSmallerMag2[linearStartIndex:linearEndIndex]))
 for i in range(linearStartIndex, linearEndIndex):
-    fitPoints.append(p[0]*magnitudes[i]+p[1])
+    fitPoints.append(p[0]*bins1[i]+p[1])
 	
-plt.plot(magnitudes[linearStartIndex:linearEndIndex],fitPoints, label="variable r, q=2.5 , gradient = "+str(p[0]))
+plt.plot(bins1[linearStartIndex:linearEndIndex],fitPoints, label="variable r, q=2.5 , gradient = "+str(p[0]))
 plt.legend()
 print("the gradient of the linear part is ",p[0])
 
